c43214ccdcd42185431e2e093123a6a737821e69
Jake Headley <jheadley@roomkey.com>
2017-04-10 21:22:19 -0600

    Committing working Node server

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
 
+app.use(express.static('assets'));
+app.use(bodyParser.json());
+
+var messages = [];
+
+app.get('/messages', function(req, res, next){
+  res.status(200).json({messages:messages})
+});
+
+app.post('/messages', function (req, res, next){
+  messages.push({ message:req.body.messages, time: new Date()});
+  res.status(200).json({message:messages});
+})
+
+app.listen(3000);
